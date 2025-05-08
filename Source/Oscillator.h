3d3f5098c642a9327f8c75cf301cf4400c7e001c
Piotr Milewski <milewski.piotr.m@gmail.com>
2025-05-08 14:51:00 +0200

    added digital resonator osc as a note

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -27,3 +27,35 @@ public:
     }
 
 };
+
+//sine osc based on digital resonator concept
+#if 0
+class Oscillator
+{
+public:
+    float amplitude;
+    float inc;
+    float phase;
+
+    float sin0;
+    float sin1;
+    float dsin;
+
+    void reset()
+    {
+        phase = 0;
+
+        sin0 = amplitude * std::sin(phase * TWO_PI);
+        sin1 = amplitude * std::sin((phase - inc) * TWO_PI);
+        dsin = 2.0f * std::cos(inc * TWO_PI);
+    }
+
+    float nextSample()
+    {
+        float sinx = dsin * sin0 - sin1;
+        sin1 = sin0;
+        sin0 = sinx;
+        return sinx;
+    }
+};
+#endif
