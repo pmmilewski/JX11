686fd8435e3ebf343b3bdb2e0404438444e26177
Piotr Milewski <milewski.piotr.m@gmail.com>
2025-05-09 15:12:42 +0200

    bandlimited impulse train

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -1,52 +1,63 @@
 #pragma once
+
 #include <cmath>
 
 constexpr float TWO_PI = 6.2831853071795864f;
+constexpr float PI = 3.1415926535897932f;
+constexpr float PI_OVER_4 = 0.7853981633974483f;
 
 class Oscillator
 {
 public:
-    float amplitude;
-    float inc;
-    float phase;
-
-    float freq;
-    float sampleRate;
-    float phaseBL;
+    float period = 0.0f;
+    float amplitude = 1.0f;
 
     void reset()
     {
+        inc = 0.0f;
         phase = 0.0f;
-        phaseBL = -0.5f;
     }
 
-    float nextBandlimitedSample()
+    float nextSample()
     {
-        phaseBL += inc;
-
-        if (phaseBL >= 1.0f)
-        {
-            phaseBL -= 1.0f;
-        }
-
         float output = 0.0f;
-        float nyquist = sampleRate / 2.0f;
-        float h = freq;
-        float i = 1.0f;
-        float m = 0.6366197724f; // 2/pi
-        while (h < nyquist)
+
+        phase += inc;
+
+        if (phase <= PI_OVER_4)
         {
-            output += m * std::sin(TWO_PI * phaseBL * i) / i;
-            h += freq;
-            i += 1.0f;
-            m = -m;
+            float halfPeriod = period / 2.0f;
+            phaseMax = std::floor(0.5f + halfPeriod) - 0.5f;
+            phaseMax *= PI;
+
+            inc = phaseMax / halfPeriod;
+            phase = -phase;
+
+            if (phase*phase > 1e-9)
+            {
+                output = amplitude * std::sin(phase) / phase;
+            }
+            else
+            {
+                output = amplitude;
+            }
+        }
+        else
+        {
+            if (phase > phaseMax)
+            {
+                phase = phaseMax + phaseMax - phase;
+                inc = -inc;
+            }
+
+            output = amplitude * std::sin(phase) / phase;
         }
 
         return output;
     }
 
-    float nextSample()
-    {
-        return  amplitude * nextBandlimitedSample();
-    }
+private:
+    float phase = 0;
+    float phaseMax = 0;
+    float inc = 0;
 };
